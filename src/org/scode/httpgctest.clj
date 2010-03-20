98b393b90b58f202782ad03d6fe9c8c2837dc50a
Peter Schuller <peter.schuller@infidyne.com>
2010-03-21 00:34:25 +0100

    Try to generate a non-trivial stack with some heap refs, and sleep in leaf.

--- src/org/scode/httpgctest.clj
+++ src/org/scode/httpgctest.clj
@@ -8,9 +8,16 @@
                     FileInputStream
                     File)))
 
+(defn gen-recursive-garbage [lvl garbage]
+  (if (> 0 lvl)
+    (gen-recursive-garbage (dec lvl) {:some-nice-garbage [1 2 3 4 5 6 7 8 9 10]})
+    (do
+      (Thread/sleep (rand 1000))
+      garbage)))
+
 (defn serve-gengarbage [request]
   (do
-    (Thread/sleep (rand 1000))
+    (gen-recursive-garbage 50 [])
     {:status 200
      :headers {}
      :body "Garbage all around."}))
