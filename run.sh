b1ad2601b0be1ef7abbe0856dd7b3cae78386cce
Peter Schuller <peter.schuller@infidyne.com>
2010-03-21 19:29:01 +0100

    Structure run.sh for easy commenting/editing of jvm options.

--- run.sh
+++ run.sh
@@ -2,4 +2,18 @@
 
 # Assumes a built uber-jar exists in current directory, and that JAVA_HOME is set.
 
-$JAVA_HOME/bin/java -XX:+PrintGC -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+UseG1GC -XX:MaxGCPauseMillis=10 -XX:GCPauseIntervalMillis=20 -XX:+G1ParallelRSetUpdatingEnabled -XX:+G1ParallelRSetScanningEnabled -XX:+PrintGCDetails -jar httpgctest-standalone.jar
+jvmopts () {
+    echo -XX:+PrintGC
+    echo -XX:+UnlockExperimentalVMOptions
+    echo -XX:+UnlockDiagnosticVMOptions
+    echo -XX:+UseG1GC
+    echo -XX:MaxGCPauseMillis=10
+    echo -XX:GCPauseIntervalMillis=15
+    echo -XX:+G1ParallelRSetUpdatingEnabled
+    echo -XX:+G1ParallelRSetScanningEnabled
+    echo -Xmx50M
+    echo -XX:G1ConfidencePercent=100
+    echo -Xss256k
+}
+
+$JAVA_HOME/bin/java $(jvmopts) -jar httpgctest-standalone.jar
