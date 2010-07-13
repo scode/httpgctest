75e232a60c6d6fe61927acab19305ffb2617586f
Peter Schuller <peter.schuller@infidyne.com>
2010-07-13 22:02:33 +0200

    Remove leiningen project def.

--- project.clj
+++
@@ -1,5 +0,0 @@
-(defproject httpgctest "0.0.1"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 [org.clojars.liebke/compojure "0.3.1-master"]]
-  :main org.scode.httpgctest)
